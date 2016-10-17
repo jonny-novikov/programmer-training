a1cae685d25a0a2f5c144d74f7db3287c003d13e
MichaelDong32 <mjdong32@gmail.com>
2016-10-17 22:04:06 +1300

    Odd number filter function

--- functional/Scratch-Functions/filter.js
+++ functional/Scratch-Functions/filter.js
@@ -29,3 +29,38 @@ function filta (func, arr) {
 //expected [2,4,6,8,10]
 
 filta(isEven, array)
+
+
+//filter function for taking out odd numbers
+
+/* declare const to go into functions
+write out expected result
+write out function
+is a guard statement needed? Include that
+declare the head and tail of the array
+what are you going to return - pass the head into the function
+loop it recursively by passing the tail into the filter function
+until finished - empty array / guard statement met
+use a ternary to find out if condition is true or false
+call the function*/
+
+const array = [1,2,3,4,5,6,7,8,9,10]
+const oddOneOut = n => n % 2 ===1
+
+const filta = (func, arr) => {
+	if (isEmpty(arr)) {
+		return []
+	}
+
+	const [x, ...xs] = arr
+
+	return func (x)
+	? [func(x), ...filta(func, xs)]
+	: filta(func, xs)
+}
+
+// expected [1,3,5,7,9]
+
+filta(oddOneOut, array)
+
+
